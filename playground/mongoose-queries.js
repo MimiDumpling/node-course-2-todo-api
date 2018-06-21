8abff0bb8803648693298b8f68bc7cea22b1671f
MimiDumpling <tienmiminguyen@gmail.com>
2018-06-21 15:25:25 -0700

    add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,40 @@
+const { ObjectId } = require('mongodb');
+
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+const { User } = require('./../server/models/user');
+
+// var id = '5b2c158a98aca5bbc62df7f2';
+
+// if(!ObjectId.isValid(id)) {
+//   console.log('Id not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('ID not found');
+//   }
+//   console.log('Todo by ID', todo);
+// }).catch((e) => console.log(e));
+
+User.findById('5b2aec04c38d31feaac9afba').then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
